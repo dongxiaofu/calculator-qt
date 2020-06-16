70f3b8c176d056b423200214983e0ce7c556a76c
cg <chuganghong@qq.com>
2020-06-16 16:23:28 +0800

    登陆框完成

---
+++ calculator/calculator_zh_CN.ts
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="calculator_zh_CN">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="dialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="26"/>
+        <source>登录到主窗口</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="42"/>
+        <source>请输入用户名</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="58"/>
+        <source>请输入密码</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="71"/>
+        <source>用户名:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="84"/>
+        <source>密码：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="97"/>
+        <source>登录</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.ui" line="110"/>
+        <source>退出</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="21"/>
+        <source>cg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="22"/>
+        <source>123456</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="26"/>
+        <source>Waring</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog.cpp" line="27"/>
+        <source>user name or password error!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="30"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="45"/>
+        <source>Hello</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="50"/>
+        <source>world</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="61"/>
+        <source>1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="66"/>
+        <source>2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
